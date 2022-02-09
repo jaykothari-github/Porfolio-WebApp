6f054ebb94dd9077ec40b8d2dfb24b601b298e87
Jay Kothari <jaykothari49@gmail.com>
2022-02-09 20:15:24 +0530

    project upload complete

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('add-project/',views.add_project, name='add-project'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from urllib.request import Request
+from django.shortcuts import redirect, render
 from .models import Project
 
 # Create your views here.
 
 def index(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all()[::-1]
     return render(request,'index.html',{'projects':projects})
+
+def add_project(request):
+    if request.method == 'POST':
+        Project.objects.create(
+            title = request.POST['title'],
+            des = request.POST['des'],
+            tech = request.POST['tech'],
+            image = request.FILES['image'] if 'image' in request.FILES else None
+        )
+        projects = Project.objects.all()[::-1]
+        return render(request,'index.html',{'projects':projects})
